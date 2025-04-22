d65f0ddcd6361adf8fb056a80307ad526f0d87c7
Fluffy-Bunny-23 <193831219+Fluffy-Bunny-23@users.noreply.github.com>
2025-04-22 16:40:49 -0700

    Update loginTEST.js

--- Code/loginTEST.js
+++ Code/loginTEST.js
@@ -52,7 +52,7 @@
     }
 
     fetch(
-      "https://raw.githubusercontent.com/Fluffy-Bunny-23/Yap-Window/refs/heads/main/Code/guiTEST.js?token=$(date +%s)",
+      "https://raw.githubusercontent.com/Fluffy-Bunny-23/Yap-Window/refs/heads/Fluffy-Bunny-23-patch-favicon-refresh-button/Code/guiTEST.js",
     )
       .then((r) => r.text())
       .then((code) => {
@@ -74,7 +74,7 @@
             child,
           };
           fetch(
-            "https://raw.githubusercontent.com/Fluffy-Bunny-23/Yap-Window/refs/heads/main/Code/chatTEST.js?token=$(date +%s)",
+            "https://raw.githubusercontent.com/Fluffy-Bunny-23/Yap-Window/refs/heads/Fluffy-Bunny-23-patch-favicon-refresh-button/Code/chatTEST.js",
           )
             .then((r) => r.text())
             .then((chatCode) => {
